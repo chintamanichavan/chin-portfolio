b6b80059d7bbb3599f4bfb6d73fc2580aa68494c
chintamanichavan <chintamani.chavan08@gmail.com>
2024-03-15 20:30:28 -0500

    updates - 03/15/2024

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeaderComponent {
+  scrollTo(section: string) {
+    const element = document.querySelector(`app-${section}`);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
   }
-
 }
